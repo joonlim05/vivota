15ded77525f7bef172616ae2c5c8d8d40946b9ab
m3llo <choojoonlim@gmail.com>
2025-11-04 13:29:14 +0800

    create basic intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import HeroSection from "@/components/hero_section";
+import IntroSection from "@/components/intro_section";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <HeroSection/>
+      <IntroSection/>
     </>
   );
 }
---
+++ components/hero_section.tsx
@@ -0,0 +1,9 @@
+export default function HeroSection() {
+    return (
+        <>
+            <section className="h-screen flex flex-col justify-center items-center text-center to-black text-white">
+                <p>WORK IN PROGRESS</p>
+            </section>
+        </>
+    );
+}
