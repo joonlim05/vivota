d1a742d24d65a4ca73a91abd37c3a4637ee81f62
m3llo <choojoonlim@gmail.com>
2025-11-04 23:16:19 +0800

    create two block layout for hero section

--- components/hero_section.tsx
+++ components/hero_section.tsx
@@ -1,8 +1,14 @@
 export default function HeroSection() {
     return (
         <>
-            <section className="h-screen flex flex-col justify-center items-center text-center to-black text-white">
-                <p>WORK IN PROGRESS</p>
+            <section className="h-screen flex flex-col md:flex-row items-center justify-center w-full text-white">
+                <p className="md:w-1/2 ml-12 flex justify-center text-3xl">
+                    Leading Tuition Centre in Singapore<br />
+                    Boost your grades,<br />
+                    Stay in a fun student community<br />
+                    Empowering students to reach their peak potential<br />
+                    with top-quality, holistic education.</p>
+                    <img src="hero_trial.jpg" className="md: w-1/2 transform scale-75 rounded-xl mr-12"></img>
             </section>
         </>
     );
